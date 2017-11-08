efc2ef234af9af98b8f724c13b06da0e9bd0b9dc
Emilio Ponce Alcalde <eponce@meteo.cat>
2017-11-08 13:18:10 +0100

    cambio a inglés

---
+++ src/Word.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Word extends Component {
+
+render() {
+  return (<div className="word">
+      <div className="word-name">Palabra</div>
+      <div className="word-description">Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit​​, morbo vel maleficia? De Apocalypsi undead dictum mauris. Hi mortuis soulless creaturas, imo monstra adventus vultus comedat cerebella viventium. Qui offenderit rapto, terribilem incessu. The voodoo sacerdos suscitat mortuos comedere carnem. Search for solum oculi eorum defunctis cerebro.</div>
+  </div>)
+  }
+}
+
+export default Word;

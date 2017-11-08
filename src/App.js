938abca9219a85654f4f0c83579b0507bb515984
Emilio Ponce Alcalde <eponce@meteo.cat>
2017-11-08 12:48:44 +0100

    eliminados recursos innecesarios de bootstrapping. Añadido soporte CSS-GRID con primer componente react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Palabra from './Palabra';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <div id="App">
+        <header>header</header>
+        <div className="left-sidebar">izquierda</div>
+        <main>
+        <Palabra/>
+        </main>
+        <div className="right-sidebar">derecha</div>
+        <footer>footer</footer>
+     </div>);
   }
 }
 
